e56001b79fe19df722e622d86d11bdbba7cf1136
Dmitry <79811924115@yandex.ru>
2024-10-03 19:33:54 +0300

    task2 OK

--- src/js/sortByHelth.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-export default function sortByHelth(heros) {
-  heros.sort((prev, next) => prev.health - next.health);
-  heros.reverse()
-  console.log(heros);
-  return heros;
-}
